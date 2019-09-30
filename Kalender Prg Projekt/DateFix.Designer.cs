cdbff93f97f0192b18ec768e98804138b16daa1e
valentin.luecke <valentin.luecke@gmx.de>
2019-09-30 11:03:54 +0200

    Die Account Page mit anzeigen der Userinformation begonnen zu bearbeiten sowie die Erweiterung des Signup fensters

--- Kalender Prg Projekt/DateFix.Designer.cs
+++ Kalender Prg Projekt/DateFix.Designer.cs
@@ -35,15 +35,27 @@
             this.appointmentTabPage1 = new System.Windows.Forms.TabPage();
             this.taskTabPage1 = new System.Windows.Forms.TabPage();
             this.accountTabPage1 = new System.Windows.Forms.TabPage();
+            this.accountLoginPanel1 = new System.Windows.Forms.Panel();
+            this.titleAccountSignInLabel1 = new System.Windows.Forms.Label();
             this.signUpAccountLinkLabel1 = new System.Windows.Forms.LinkLabel();
             this.signInAccountButton1 = new System.Windows.Forms.Button();
             this.passwordAccountTextbox1 = new System.Windows.Forms.TextBox();
             this.usernameAccountTextbox1 = new System.Windows.Forms.TextBox();
             this.passwordAccountLabel1 = new System.Windows.Forms.Label();
             this.usernameAccountLabel1 = new System.Windows.Forms.Label();
-            this.titleAccountSignInLabel1 = new System.Windows.Forms.Label();
+            this.accountInformationsPanel1 = new System.Windows.Forms.Panel();
+            this.usernameInformationLabel1 = new System.Windows.Forms.Label();
+            this.firstnameInformationLabel1 = new System.Windows.Forms.Label();
+            this.lastnameInforamtionLabel1 = new System.Windows.Forms.Label();
+            this.birthdateInformationLabel1 = new System.Windows.Forms.Label();
+            this.usernameInformationTextBox1 = new System.Windows.Forms.TextBox();
+            this.firstnameInformationTextBox1 = new System.Windows.Forms.TextBox();
+            this.lastnameInforamtionTextBox1 = new System.Windows.Forms.TextBox();
+            this.birthdateInformationTextBox1 = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.accountTabPage1.SuspendLayout();
+            this.accountLoginPanel1.SuspendLayout();
+            this.accountInformationsPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -100,13 +112,8 @@
             // 
             // accountTabPage1
             // 
-            this.accountTabPage1.Controls.Add(this.titleAccountSignInLabel1);
-            this.accountTabPage1.Controls.Add(this.signUpAccountLinkLabel1);
-            this.accountTabPage1.Controls.Add(this.signInAccountButton1);
-            this.accountTabPage1.Controls.Add(this.passwordAccountTextbox1);
-            this.accountTabPage1.Controls.Add(this.usernameAccountTextbox1);
-            this.accountTabPage1.Controls.Add(this.passwordAccountLabel1);
-            this.accountTabPage1.Controls.Add(this.usernameAccountLabel1);
+            this.accountTabPage1.Controls.Add(this.accountLoginPanel1);
+            this.accountTabPage1.Controls.Add(this.accountInformationsPanel1);
             this.accountTabPage1.Location = new System.Drawing.Point(4, 22);
             this.accountTabPage1.Name = "accountTabPage1";
             this.accountTabPage1.Size = new System.Drawing.Size(792, 424);
@@ -114,67 +121,167 @@
             this.accountTabPage1.Text = "Konto";
             this.accountTabPage1.UseVisualStyleBackColor = true;
             // 
+            // accountLoginPanel1
+            // 
+            this.accountLoginPanel1.Controls.Add(this.titleAccountSignInLabel1);
+            this.accountLoginPanel1.Controls.Add(this.signUpAccountLinkLabel1);
+            this.accountLoginPanel1.Controls.Add(this.signInAccountButton1);
+            this.accountLoginPanel1.Controls.Add(this.passwordAccountTextbox1);
+            this.accountLoginPanel1.Controls.Add(this.usernameAccountTextbox1);
+            this.accountLoginPanel1.Controls.Add(this.passwordAccountLabel1);
+            this.accountLoginPanel1.Controls.Add(this.usernameAccountLabel1);
+            this.accountLoginPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.accountLoginPanel1.Location = new System.Drawing.Point(396, 0);
+            this.accountLoginPanel1.Name = "accountLoginPanel1";
+            this.accountLoginPanel1.Size = new System.Drawing.Size(396, 424);
+            this.accountLoginPanel1.TabIndex = 14;
+            // 
+            // titleAccountSignInLabel1
+            // 
+            this.titleAccountSignInLabel1.AutoSize = true;
+            this.titleAccountSignInLabel1.Location = new System.Drawing.Point(68, 62);
+            this.titleAccountSignInLabel1.Name = "titleAccountSignInLabel1";
+            this.titleAccountSignInLabel1.Size = new System.Drawing.Size(33, 13);
+            this.titleAccountSignInLabel1.TabIndex = 19;
+            this.titleAccountSignInLabel1.Text = "Login";
+            // 
             // signUpAccountLinkLabel1
             // 
             this.signUpAccountLinkLabel1.AutoSize = true;
-            this.signUpAccountLinkLabel1.Location = new System.Drawing.Point(532, 216);
+            this.signUpAccountLinkLabel1.Location = new System.Drawing.Point(68, 195);
             this.signUpAccountLinkLabel1.Name = "signUpAccountLinkLabel1";
             this.signUpAccountLinkLabel1.Size = new System.Drawing.Size(94, 13);
-            this.signUpAccountLinkLabel1.TabIndex = 11;
+            this.signUpAccountLinkLabel1.TabIndex = 18;
             this.signUpAccountLinkLabel1.TabStop = true;
             this.signUpAccountLinkLabel1.Text = "Jetzt Registrieren?";
+            this.signUpAccountLinkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.SignUpAccountLinkLabel1_LinkClicked);
             // 
             // signInAccountButton1
             // 
-            this.signInAccountButton1.Location = new System.Drawing.Point(535, 181);
+            this.signInAccountButton1.Location = new System.Drawing.Point(71, 160);
             this.signInAccountButton1.Name = "signInAccountButton1";
             this.signInAccountButton1.Size = new System.Drawing.Size(204, 23);
-            this.signInAccountButton1.TabIndex = 10;
+            this.signInAccountButton1.TabIndex = 17;
             this.signInAccountButton1.Text = "Anmelden";
             this.signInAccountButton1.UseVisualStyleBackColor = true;
             this.signInAccountButton1.Click += new System.EventHandler(this.SignInAccountButton1_Click);
             // 
             // passwordAccountTextbox1
             // 
-            this.passwordAccountTextbox1.Location = new System.Drawing.Point(639, 145);
+            this.passwordAccountTextbox1.Location = new System.Drawing.Point(175, 124);
             this.passwordAccountTextbox1.Name = "passwordAccountTextbox1";
             this.passwordAccountTextbox1.Size = new System.Drawing.Size(100, 20);
-            this.passwordAccountTextbox1.TabIndex = 9;
+            this.passwordAccountTextbox1.TabIndex = 16;
             this.passwordAccountTextbox1.UseSystemPasswordChar = true;
             // 
             // usernameAccountTextbox1
             // 
-            this.usernameAccountTextbox1.Location = new System.Drawing.Point(639, 117);
+            this.usernameAccountTextbox1.Location = new System.Drawing.Point(175, 96);
             this.usernameAccountTextbox1.Name = "usernameAccountTextbox1";
             this.usernameAccountTextbox1.Size = new System.Drawing.Size(100, 20);
-            this.usernameAccountTextbox1.TabIndex = 8;
+            this.usernameAccountTextbox1.TabIndex = 15;
             // 
             // passwordAccountLabel1
             // 
             this.passwordAccountLabel1.AutoSize = true;
-            this.passwordAccountLabel1.Location = new System.Drawing.Point(532, 148);
+            this.passwordAccountLabel1.Location = new System.Drawing.Point(68, 127);
             this.passwordAccountLabel1.Name = "passwordAccountLabel1";
             this.passwordAccountLabel1.Size = new System.Drawing.Size(50, 13);
-            this.passwordAccountLabel1.TabIndex = 7;
+            this.passwordAccountLabel1.TabIndex = 14;
             this.passwordAccountLabel1.Text = "Passwort";
             // 
             // usernameAccountLabel1
             // 
             this.usernameAccountLabel1.AutoSize = true;
-            this.usernameAccountLabel1.Location = new System.Drawing.Point(532, 120);
+            this.usernameAccountLabel1.Location = new System.Drawing.Point(68, 99);
             this.usernameAccountLabel1.Name = "usernameAccountLabel1";
             this.usernameAccountLabel1.Size = new System.Drawing.Size(75, 13);
-            this.usernameAccountLabel1.TabIndex = 6;
+            this.usernameAccountLabel1.TabIndex = 13;
             this.usernameAccountLabel1.Text = "Benutzername";
             // 
-            // titleAccountSignInLabel1
+            // accountInformationsPanel1
             // 
-            this.titleAccountSignInLabel1.AutoSize = true;
-            this.titleAccountSignInLabel1.Location = new System.Drawing.Point(532, 83);
-            this.titleAccountSignInLabel1.Name = "titleAccountSignInLabel1";
-            this.titleAccountSignInLabel1.Size = new System.Drawing.Size(33, 13);
-            this.titleAccountSignInLabel1.TabIndex = 12;
-            this.titleAccountSignInLabel1.Text = "Login";
+            this.accountInformationsPanel1.Controls.Add(this.birthdateInformationTextBox1);
+            this.accountInformationsPanel1.Controls.Add(this.lastnameInforamtionTextBox1);
+            this.accountInformationsPanel1.Controls.Add(this.firstnameInformationTextBox1);
+            this.accountInformationsPanel1.Controls.Add(this.usernameInformationTextBox1);
+            this.accountInformationsPanel1.Controls.Add(this.birthdateInformationLabel1);
+            this.accountInformationsPanel1.Controls.Add(this.lastnameInforamtionLabel1);
+            this.accountInformationsPanel1.Controls.Add(this.firstnameInformationLabel1);
+            this.accountInformationsPanel1.Controls.Add(this.usernameInformationLabel1);
+            this.accountInformationsPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.accountInformationsPanel1.Location = new System.Drawing.Point(0, 0);
+            this.accountInformationsPanel1.Name = "accountInformationsPanel1";
+            this.accountInformationsPanel1.Size = new System.Drawing.Size(396, 424);
+            this.accountInformationsPanel1.TabIndex = 13;
+            // 
+            // usernameInformationLabel1
+            // 
+            this.usernameInformationLabel1.AutoSize = true;
+            this.usernameInformationLabel1.Location = new System.Drawing.Point(35, 62);
+            this.usernameInformationLabel1.Name = "usernameInformationLabel1";
+            this.usernameInformationLabel1.Size = new System.Drawing.Size(75, 13);
+            this.usernameInformationLabel1.TabIndex = 0;
+            this.usernameInformationLabel1.Text = "Benutzername";
+            // 
+            // firstnameInformationLabel1
+            // 
+            this.firstnameInformationLabel1.AutoSize = true;
+            this.firstnameInformationLabel1.Location = new System.Drawing.Point(35, 100);
+            this.firstnameInformationLabel1.Name = "firstnameInformationLabel1";
+            this.firstnameInformationLabel1.Size = new System.Drawing.Size(49, 13);
+            this.firstnameInformationLabel1.TabIndex = 1;
+            this.firstnameInformationLabel1.Text = "Vorname";
+            // 
+            // lastnameInforamtionLabel1
+            // 
+            this.lastnameInforamtionLabel1.AutoSize = true;
+            this.lastnameInforamtionLabel1.Location = new System.Drawing.Point(35, 135);
+            this.lastnameInforamtionLabel1.Name = "lastnameInforamtionLabel1";
+            this.lastnameInforamtionLabel1.Size = new System.Drawing.Size(59, 13);
+            this.lastnameInforamtionLabel1.TabIndex = 2;
+            this.lastnameInforamtionLabel1.Text = "Nachname";
+            // 
+            // birthdateInformationLabel1
+            // 
+            this.birthdateInformationLabel1.AutoSize = true;
+            this.birthdateInformationLabel1.Location = new System.Drawing.Point(35, 172);
+            this.birthdateInformationLabel1.Name = "birthdateInformationLabel1";
+            this.birthdateInformationLabel1.Size = new System.Drawing.Size(73, 13);
+            this.birthdateInformationLabel1.TabIndex = 3;
+            this.birthdateInformationLabel1.Text = "Geburtsdatum";
+            // 
+            // usernameInformationTextBox1
+            // 
+            this.usernameInformationTextBox1.Location = new System.Drawing.Point(158, 59);
+            this.usernameInformationTextBox1.Name = "usernameInformationTextBox1";
+            this.usernameInformationTextBox1.ReadOnly = true;
+            this.usernameInformationTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.usernameInformationTextBox1.TabIndex = 4;
+            // 
+            // firstnameInformationTextBox1
+            // 
+            this.firstnameInformationTextBox1.Location = new System.Drawing.Point(158, 97);
+            this.firstnameInformationTextBox1.Name = "firstnameInformationTextBox1";
+            this.firstnameInformationTextBox1.ReadOnly = true;
+            this.firstnameInformationTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.firstnameInformationTextBox1.TabIndex = 5;
+            // 
+            // lastnameInforamtionTextBox1
+            // 
+            this.lastnameInforamtionTextBox1.Location = new System.Drawing.Point(158, 132);
+            this.lastnameInforamtionTextBox1.Name = "lastnameInforamtionTextBox1";
+            this.lastnameInforamtionTextBox1.ReadOnly = true;
+            this.lastnameInforamtionTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.lastnameInforamtionTextBox1.TabIndex = 6;
+            // 
+            // birthdateInformationTextBox1
+            // 
+            this.birthdateInformationTextBox1.Location = new System.Drawing.Point(158, 169);
+            this.birthdateInformationTextBox1.Name = "birthdateInformationTextBox1";
+            this.birthdateInformationTextBox1.ReadOnly = true;
+            this.birthdateInformationTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.birthdateInformationTextBox1.TabIndex = 7;
             // 
             // DateFix
             // 
@@ -188,7 +295,10 @@
             this.Load += new System.EventHandler(this.DateFix_Load);
             this.tabControl1.ResumeLayout(false);
             this.accountTabPage1.ResumeLayout(false);
-            this.accountTabPage1.PerformLayout();
+            this.accountLoginPanel1.ResumeLayout(false);
+            this.accountLoginPanel1.PerformLayout();
+            this.accountInformationsPanel1.ResumeLayout(false);
+            this.accountInformationsPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -201,6 +311,7 @@
         private System.Windows.Forms.TabPage taskTabPage1;
         private System.Windows.Forms.TabPage accountTabPage1;
         private System.Windows.Forms.TabPage contactsTabPage1;
+        private System.Windows.Forms.Panel accountLoginPanel1;
         private System.Windows.Forms.Label titleAccountSignInLabel1;
         private System.Windows.Forms.LinkLabel signUpAccountLinkLabel1;
         private System.Windows.Forms.Button signInAccountButton1;
@@ -208,5 +319,14 @@
         private System.Windows.Forms.TextBox usernameAccountTextbox1;
         private System.Windows.Forms.Label passwordAccountLabel1;
         private System.Windows.Forms.Label usernameAccountLabel1;
+        private System.Windows.Forms.Panel accountInformationsPanel1;
+        private System.Windows.Forms.Label birthdateInformationLabel1;
+        private System.Windows.Forms.Label lastnameInforamtionLabel1;
+        private System.Windows.Forms.Label firstnameInformationLabel1;
+        private System.Windows.Forms.Label usernameInformationLabel1;
+        private System.Windows.Forms.TextBox birthdateInformationTextBox1;
+        private System.Windows.Forms.TextBox lastnameInforamtionTextBox1;
+        private System.Windows.Forms.TextBox firstnameInformationTextBox1;
+        private System.Windows.Forms.TextBox usernameInformationTextBox1;
     }
 }
--- Kalender Prg Projekt/DateFix.cs
+++ Kalender Prg Projekt/DateFix.cs
@@ -13,11 +13,16 @@ namespace Kalender_Prg_Projekt
 {
     public partial class DateFix : Form
     {
-        int activeUserID = 0;
+        public int activeUserID = 0; //wenn der wert auf Null ist, ist kein nutzer eingeloggt. Sobald sich ein Nutzer einloggt wird der Wert auf die Nutzer ID Gesetzt
+        string query = "";
 
         public DateFix()
         {
             InitializeComponent();
+            if(activeUserID == 0)
+            {
+                accountInformationsPanel1.Hide();
+            }
         }
 
         private void DateFix_Load(object sender, EventArgs e)
@@ -29,9 +34,25 @@ namespace Kalender_Prg_Projekt
             activeUserID = dummy;
         }
 
+        public void showAccountInformation()
+        {
+            accountLoginPanel1.Hide();
+            accountInformationsPanel1.Show();
+            this.query = $"SELECT Username FROM tbl_user WHERE tbl_user.ID = '{this.activeUserID}'";
+            usernameInformationTextBox1.Text = SQL_Query.Query_String(query);
+            this.query = $"SELECT Firstname FROM tbl_user WHERE tbl_user.ID = '{this.activeUserID}'";
+            firstnameInformationTextBox1.Text = SQL_Query.Query_String(query);
+            this.query = $"SELECT Lastname FROM tbl_user WHERE tbl_user.ID = '{this.activeUserID}'";
+            lastnameInforamtionTextBox1.Text = SQL_Query.Query_String(query);
+            this.query = $"SELECT Birthdate FROM tbl_user WHERE tbl_user.ID = '{this.activeUserID}'";
+            birthdateInformationTextBox1.Text = SQL_Query.Query_String(query);
+        }
+
+
+
         private void SignInAccountButton1_Click(object sender, EventArgs e)
         {
-            string query = $"SELECT * FROM tbl_user WHERE tbl_user.Username = '{usernameAccountTextbox1.Text}'";
+            this.query = $"SELECT * FROM tbl_user WHERE tbl_user.Username = '{usernameAccountTextbox1.Text}'";
             if(SQL_Query.Query_Compare(query))
             {
                 if (Password.checkHashedPassword(passwordAccountTextbox1.Text, usernameAccountTextbox1.Text))
@@ -39,6 +60,7 @@ namespace Kalender_Prg_Projekt
                     Console.Write("Sign In successfull");
                     query = $"SELECT ID FROM tbl_user WHERE tbl_user.Username = '{usernameAccountTextbox1.Text}'";
                     setActiveUserID(SQL_Query.Query_Int(query));
+                    showAccountInformation();
                 }
                 else
                 {
@@ -49,6 +71,13 @@ namespace Kalender_Prg_Projekt
             {
                 MessageBox.Show("Username not found");
             }
+        }
+
+        private void SignUpAccountLinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Sign_Up signup = new Sign_Up();
+            //this.Hide();
+            signup.ShowDialog();
         }
     }
 }
